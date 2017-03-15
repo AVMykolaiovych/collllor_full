8dbb9ac3e7b12c3a5a5e367c1ff849e0fd4b4c86
ROXy <djeffik@gmail.com>
2017-03-15 09:58:52 +0200

    final commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
-$(function() {
-
-	// Custom JS
-
+$(document).ready(function(){
+    $(document).mousemove(function(e){
+        var opacity = 1;
+        $('#purple-bg').css('opacity', ''+(opacity - e.pageY/1000));
+    });
 });
